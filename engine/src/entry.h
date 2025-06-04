4933f950d000f05fcea88f84c4790cffe0cd1a30
JuanJose Gomez <gjuanjo1999@gmail.com>
2025-06-04 14:38:25 +0200

    Added missing file for architecture program-core

--- engine/src/entry.h
+++ engine/src/entry.h
@@ -1,20 +1,23 @@
 #pragma once
 
-#include "./core/application.h"
+#include "core/application.h"
+#include "program_types.h"
 
 // Externally-defined function to create a program
-extern bool create_program();
+extern bool create_program(caliope::program_config& out_config);
 
 /**
  * Main entry point of the program
  */
 int main(void){
 
-    if(!create_program()){
+    caliope::program_config config;
+
+    if(!create_program(config)){
         return -1;
     }
 
-    if (!caliope::application_create()) {
+    if (!caliope::application_create(&config)) {
         return -1;
     }
     
