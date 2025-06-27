632e4438d97620bf36e9c9c50b99135bda15de7e
JuanJose Gomez <gjuanjo1999@gmail.com>
2025-06-28 01:31:48 +0200

    Builds dependencies when downloads it, copy all assets to the .exe directories

--- PostBuild.bat
+++ PostBuild.bat
@@ -9,6 +9,8 @@ echo "assets/shaders/Builtin.SpriteShader.vert.glsl -> assets/shaders/Builtin.Sp
 echo "assets/shaders/Builtin.SpriteShader.frag.glsl -> assets/shaders/Builtin.SpriteShader.frag.spv"
 %VULKAN_SDK%\bin\glslc.exe -fshader-stage=frag assets/shaders/Builtin.SpriteShader.frag.glsl -o assets/shaders/Builtin.SpriteShader.frag.spv
 
+xcopy /s /y /q /I assets bin\Debug\assets
+xcopy /s /y /q /I assets build\assets
 
 echo "Done."
 
